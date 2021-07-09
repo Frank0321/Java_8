5143a57f1058138b8b24c929c04f4ec7c52c92c8
Frank0321 <lefty21050@gmail.com>
2021-07-09 14:41:57 +0800

    新增 Java mevan 專案

---
+++ src/main/java/tw/com/softleader/Java_8/Java8Application.java
@@ -0,0 +1,13 @@
+package tw.com.softleader.Java_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8Application.class, args);
+	}
+
+}
---
+++ src/test/java/tw/com/softleader/Java_8/Java8ApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.softleader.Java_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
